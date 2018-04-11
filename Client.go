9eb6e6f66d3f00ec5ea804f474ee3d23f5beb461
kepung <kepung@gmail.com>
2018-04-11 22:02:56 +1200

    updated code.

--- Client.go
+++ Client.go
@@ -2,17 +2,35 @@ package goCoinGate
 
 import (
 	"github.com/parnurzeal/gorequest"
-	)
+	"fmt"
+)
 
 type CoinGateClient struct {
 	apiKey string
 	secret string
 }
 
-func Connet(key string, secret string) (int, bool) {
+func Connect(key string, secret string) (int, bool) {
 	return  1, false
 }
 
 func CreateOrder() bool {
+
+	const createUrl = "https://api-sandbox.coingate.com/v1/orders"
+
+	request := gorequest.New()
+	resp, _, errs := request.Post(createUrl).
+		Set("Notes","gorequst is coming!").
+		Send(`{"name":"backy", "species":"dog"}`).
+		End()
+
+	if (errs != nil) {
+		fmt.Print("ok to proceed")
+	}
+
+	fmt.Println(resp.Status)
+
+	//resp.Body.Read()
+
 	return true;
 }
